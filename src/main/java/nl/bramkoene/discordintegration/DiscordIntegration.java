fcc0091e353a5fc48878c7c0114c75640c7ee5e2
minestarnl <bramkoene@gmail.com>
2019-10-08 21:01:52 +0200

    Added the spigot project

---
+++ src/main/java/nl/bramkoene/discordintegration/DiscordIntegration.java
@@ -0,0 +1,17 @@
+package nl.bramkoene.discordintegration;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DiscordIntegration extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
